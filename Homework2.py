b8fa6e55e266acb292a8802ded872a86c6deb0b6
Justking 31 <kelsinbekovadilhan2018@gmail.com>
2021-04-10 16:46:13 +0600

    hw2

--- Homework2.py
+++ Homework2.py
@@ -7,40 +7,45 @@
 
 class Complex:
 
-    def __init__(self, real, imag):
+    def __init__(self, real=0, imag=0):
         self.real = real
         self.imag = imag
 
     def __add__(self, other):
-        real = self.real + other.real
-        imag = self.imag + other.imag
-        new_complex = Complex(real, imag)
-        return new_complex
+        return Complex(self.real + other.real, self.imag + other.imag)
 
     def __sub__(self, other):
-        real = self.real + other.real
-        imag = self.imag + other.imag
-        new_complex = Complex(real, imag)
-        return new_complex
+        return Complex(self.real - self.real, self.imag - other.imag)
 
     def __truediv__(self, other):
-        real = self.real + other.real
-        imag = self.imag + other.imag
-        new_complex = Complex(real, imag)
-        return new_complex
+        return Complex(self.real / other.real, self.imag / other.imag)
 
     def __mul__(self, other):
-        real = self.real + other.real
-        imag = self.imag + other.imag
+        real = self.real * other.real - self.imag * other.imag
+        imag = self.imag * other.imag + self.imag * other.imag
         new_complex = Complex(real, imag)
         return new_complex
 
-
     def __str__(self):
-        return f"{self.real} + {self.imag}i"
+        return f"{self.real} + {self.imag}i" if self.imag >= 0 else f'{self.real}{self.imag}i'
 
 
-c1 = Complex(1, 2)
-c2 = Complex(1, 4)
-c3 = c1 + c2
-print(c3)
+
+complex1 = Complex(1, 2)
+complex2 = Complex(2, 5)
+result = complex1 * complex2
+complex3 = complex1 + complex2
+complex4 = complex1 + complex2
+complex5 = complex1 - complex2
+complex6 = complex1 / complex2
+print(complex3.real)
+print(complex3.imag)
+print()
+print(complex4.real)
+print(complex4.imag)
+print()
+print(complex5.real)
+print(complex5.imag)
+print()
+print(complex6.real)
+print(complex6.imag)
